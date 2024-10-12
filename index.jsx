309fe3f0d8c2a5c7fb6f5395c2f328829b503683
Nazar Vinnichuk <nazar@vinnich.uk>
2024-10-12 19:49:26 +0300

    Set up the package.

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import {createRoot} from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(<h1>Hashue</h1>);
